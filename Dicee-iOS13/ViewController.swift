6dc311f45823c8127fa6d8f646e42224336303b8
Felipe Rojas Alfonso <ferroal304@gmail.com>
2020-03-15 13:06:26 +0100

    feat: Section 4.46 Randomise the dice images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,21 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    var diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
     @IBAction func rollButtonPress(_ sender: UIButton) {
-        diceImageView1.image = diceImages[leftDiceNumber]
-        leftDiceNumber = leftDiceNumber + 1
         
-        diceImageView2.image = diceImages[rightDiceNumber]
-        rightDiceNumber = rightDiceNumber - 1
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+
     }
     
 }
